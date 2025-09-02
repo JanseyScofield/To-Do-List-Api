903d4237b100d372cdf9d80a383b6b4f0d6cb8cf
Jansey Scofield <jfilho@reconcavo.org.br>
2025-09-02 14:47:25 -0300

    Update init database script

--- postgres-container/scripts/init.sql
+++ postgres-container/scripts/init.sql
@@ -1,24 +1,25 @@
-CREATE TABLE status(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL
+CREATE TABLE Status(
+    Id SERIAL PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE Users(
+    Id SERIAL PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE task(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL,
-    create_at TIMESTAMP NOT NULL,
-    finish_at TIMESTAMP,
-    status_id INTEGER NOT NULL,
-    responsible_id INTEGER,
-    CONSTRAINT FK_STATUS FOREIGN KEY (status_id) REFERENCES status (id),
-    CONSTRAINT FK_RESPONSIBLE FOREIGN KEY (responsible_id) REFERENCES users (id)
+CREATE TABLE Task(
+    Id SERIAL PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Description TEXT NOT NULL,
+    Create_at TIMESTAMP NOT NULL,
+    Finish_at TIMESTAMP,
+    Status_id INTEGER NOT NULL,
+    Responsible_id INTEGER,
+    CONSTRAINT FK_STATUS FOREIGN KEY (Status_id) REFERENCES status (Id),
+    CONSTRAINT FK_RESPONSIBLE FOREIGN KEY (Responsible_id) REFERENCES users (Id)
 );
+
+INSERT INTO Status(Name) VALUES ('Created'), ('InProgress'), ('Finished'), ('Canceled')
