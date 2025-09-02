1d6bdd736bef155ee7071eeac21944d94fbcccf8
Jansey Scofield <jfilho@reconcavo.org.br>
2025-09-02 14:38:17 -0300

    Add Task Domain

---
+++ ToDoList.Domain/Entities/Task.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Interfaces;
+using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class Task : IIdentifyEntity<int>
+    {
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                Utils.Validate.ValidateIntId(value);
+                _id = value;
+            }
+        }
+
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? FinishAt { get; private set; }
+        public Status Status { get; private set; }
+        public Users User { get; private set; }
+
+        public Task(string name, string description, Status status, Users user)
+        {
+            Validate(name, description);
+            Name = name;
+            Description = description;
+            CreatedAt = DateTime.Now;
+            Status = status;
+            User = user;
+        }
+
+        public void Update(string name, string description, Status status)
+        {
+            Validate(name, description);
+            Name = name;
+            Description = description;
+            Status = status;
+            if (Status == Status.Finished)
+            {
+                FinishAt = DateTime.Now;
+            }
+        }
+
+        private void Validate(string name, string description)
+        {
+            Utils.Validate.ValidateName(name);
+            Utils.Validate.ValidateDescription(description);
+        }
+    }
+}
