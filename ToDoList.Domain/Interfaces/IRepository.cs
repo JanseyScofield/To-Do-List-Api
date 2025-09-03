834b72617b61caac52a76f160de133b6f5e8cde7
Jansey Scofield <jfilho@reconcavo.org.br>
2025-09-03 15:50:10 -0300

    Add IRepository

---
+++ ToDoList.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Interfaces
+{
+    public interface IRepository<T,TEntity, TKey> where TEntity : IIdentifyEntity<TKey>
+    {
+        public Task<IEnumerable<T>> GetAll();
+        public Task<T>? GetById(TKey id);
+        public Task Create(TEntity entity);
+        public Task Update(TEntity entity);
+        public Task DeleteById(TKey id);
+    }
+}
