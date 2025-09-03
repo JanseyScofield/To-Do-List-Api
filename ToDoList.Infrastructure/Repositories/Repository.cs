e7220bbb291dc0339e9f584cf7ce61fb7937ceb3
Jansey Scofield <jfilho@reconcavo.org.br>
2025-09-03 15:50:21 -0300

    Start implements Repository Base

---
+++ ToDoList.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Interfaces;
+
+namespace ToDoList.Infrastructure.Repositories
+{
+    public class Repository<T, TEntity, TKey> : IRepository<T, TEntity, TKey> where TEntity : IIdentifyEntity<TKey> where T : IModal<TEntity, TKey>, new()
+    {
+        private AppDbContext _context;
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(TEntity entity)
+        {
+            try
+            {
+                var modal = new T();
+                modal.ConvertDomainToModel(entity);
+                await _context.Set<T>().AddAsync(modal);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error in create " + ex.Message);
+            }
+        }
+
+        public Task DeleteById(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T>? GetById(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
