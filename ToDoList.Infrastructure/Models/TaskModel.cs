145fb36148b804adf613ff1fb7c23a707d9f31b8
Jansey Scofield <jfilho@reconcavo.org.br>
2025-09-08 15:53:15 -0300

    Fix name Models

---
+++ ToDoList.Infrastructure/Models/TaskModel.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Interfaces;
+using Domain.Entities;
+
+namespace ToDoList.Infrastructure.Models
+{
+    public class TaskModel : IModel<Tasks, int>
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public DateTime CreatedAt { get; set; } = new();
+        public DateTime? FinishAt { get; set; } = new();
+        public byte StatusId { get; set; } = new();
+        public int UserId { get; set; } = new();
+        public UsersModel User { get; set; } = new();
+
+        public void ConvertDomainToModel(Tasks entity)
+        {
+            Id = entity.Id;
+            Name = entity.Name;
+            Description = entity.Description;
+            CreatedAt = entity.CreatedAt;
+            FinishAt = entity.FinishAt;
+            StatusId = (byte)entity.Status;
+            UserId = entity.User.Id;
+            User.ConvertDomainToModel(entity.User);
+        }
+    }
+}
---
+++ ToDoList.Infrastructure/Models/UsersModel.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Interfaces;
+using Domain.Entities;
+
+namespace ToDoList.Infrastructure.Models
+{
+    public class UsersModel : IModel<Users, int>
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+
+        public void ConvertDomainToModel(Users entity)
+        {
+            Id = entity.Id;
+            Name = entity.Name;
+            Email = entity.Email;
+            Password = entity.Password;
+        }
+    }
+}
